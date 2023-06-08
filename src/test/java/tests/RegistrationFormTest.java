b7fffbdb381463f6f798ebc01cbc9ae8fa24485f
Veronika Iatckaia <neznatnov@gmail.com>
2023-06-08 12:03:03 +0200

    after each

--- src/test/java/tests/RegistrationFormTest.java
+++ src/test/java/tests/RegistrationFormTest.java
@@ -1,7 +1,9 @@
 package tests;
 
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -51,4 +53,12 @@ public class RegistrationFormTest extends TestBase {
 
 
     }
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+    }
 }
+
